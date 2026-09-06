116d0c2ce6b89643c92e44e73024054f9ee70da2
baglbot8 <baglbot8@gmail.com>
2026-09-06 19:09:44 -0400

    Publish the playable web build
    Built from 74eb86a.

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1788736097|1625553';
+const CACHE_VERSION = '1788736183|1260558';
 /** @type {string} */
 const CACHE_PREFIX = 'Astro Neighbor-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
